a8c9d4ad72410e64d3193aacfa4e781da97882ae
hkh <860924509@qq.com>
2022-12-05 10:57:38 +0800

    Second commit

--- src/main/java/com/atguigu/git/GitTestJava.java
+++ src/main/java/com/atguigu/git/GitTestJava.java
@@ -9,6 +9,7 @@ public class GitTestJava {
         System.out.println("Hello git");
         System.out.println("Hello git2");
         System.out.println("Hello git3");
+        System.out.println("Hello git4");
 
 
     }
