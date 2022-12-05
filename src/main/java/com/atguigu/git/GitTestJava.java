3703cb551a10cd37711bf53780a0812c0a68ce48
hkh <860924509@qq.com>
2022-12-05 10:50:46 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTestJava.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @author 霍柯宏
+ * @version 1.0
+ */
+public class GitTestJava {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("Hello git2");
+        System.out.println("Hello git3");
+
+
+    }
+}
